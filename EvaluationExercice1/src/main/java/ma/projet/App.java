49e0e76ce66df346bc5853776159254da1250e9b
chakibamine <aminechakib2003@gmail.com>
2025-10-23 19:08:02 +0100

    first commit

---
+++ EvaluationExercice1/src/main/java/ma/projet/App.java
@@ -0,0 +1,9 @@
+package ma.projet;
+
+import ma.projet.test.TestGestionStock;
+
+public class App {
+    public static void main(String[] args) {
+        TestGestionStock.main(args);
+    }
+}
---
+++ EvaluationExercice1/src/main/java/ma/projet/test/TestGestionStock.java
@@ -0,0 +1,126 @@
+package ma.projet.test;
+
+
+import ma.projet.classes.Categorie;
+import ma.projet.classes.Commande;
+import ma.projet.classes.LigneCommande;
+import ma.projet.classes.Produit;
+import ma.projet.service.CategorieService;
+import ma.projet.service.CommandeService;
+import ma.projet.service.LigneCommandeService;
+import ma.projet.service.ProduitService;
+import ma.projet.util.HibernateUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class TestGestionStock {
+
+    public static void main(String[] args) {
+        try {
+
+            CategorieService categorieService = new CategorieService();
+            ProduitService produitService = new ProduitService();
+            CommandeService commandeService = new CommandeService();
+            LigneCommandeService ligneCommandeService = new LigneCommandeService();
+
+
+            Categorie catOrdinateurs = new Categorie("Ordinateurs", "Ordinateurs de bureau et portables");
+            Categorie catPeriph = new Categorie("Périphériques", "Souris, claviers, écrans");
+            Categorie catLogiciels = new Categorie("Logiciels", "Logiciels et applications");
+
+            categorieService.create(catOrdinateurs);
+            categorieService.create(catPeriph);
+            categorieService.create(catLogiciels);
+
+
+            Produit p1 = new Produit("ES12", "Ordinateur portable Dell", 120, 10, catOrdinateurs);
+            Produit p2 = new Produit("ZR85", "Souris optique", 100, 25, catPeriph);
+            Produit p3 = new Produit("EE85", "Écran LCD 24 pouces", 200, 5, catPeriph);
+            Produit p4 = new Produit("REF-4", "Clavier mécanique", 80, 15, catPeriph);
+            Produit p5 = new Produit("REF-5", "Logiciel antivirus", 150, 20, catLogiciels);
+
+            produitService.create(p1);
+            produitService.create(p2);
+            produitService.create(p3);
+            produitService.create(p4);
+            produitService.create(p5);
+
+
+
+            List<Produit> produitsOrdinateurs = produitService.findByCategorie(catOrdinateurs);
+            System.out.println("Produits de la catégorie Ordinateurs :");
+            for (Produit p : produitsOrdinateurs) {
+                System.out.println("- " + p.getReference() + " : " + p.getDesignation() + " (" + p.getPrix() + " DH)");
+            }
+
+            List<Produit> produitsPeriph = produitService.findByCategorie(catPeriph);
+            System.out.println("\nProduits de la catégorie Périphériques :");
+            for (Produit p : produitsPeriph) {
+                System.out.println("- " + p.getReference() + " : " + p.getDesignation() + " (" + p.getPrix() + " DH)");
+            }
+
+
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            Date date1 = sdf.parse("14/03/2013");
+            Date date2 = sdf.parse("15/03/2013");
+            Date date3 = sdf.parse("16/03/2013");
+
+            Commande cmd1 = new Commande(date1, "Commande 1");
+            Commande cmd2 = new Commande(date2, "Commande 2");
+            Commande cmd3 = new Commande(date3, "Commande 3");
+
+            commandeService.create(cmd1);
+            commandeService.create(cmd2);
+            commandeService.create(cmd3);
+
+
+
+            LigneCommande lc1 = new LigneCommande(7, 120, cmd1, p1);
+            LigneCommande lc2 = new LigneCommande(14, 100, cmd1, p2);
+            LigneCommande lc3 = new LigneCommande(5, 200, cmd1, p3);
+
+            LigneCommande lc4 = new LigneCommande(7, 1000, cmd2, p5);
+            LigneCommande lc5 = new LigneCommande(1000, 10, cmd2, p4);
+
+            ligneCommandeService.create(lc1);
+            ligneCommandeService.create(lc2);
+            ligneCommandeService.create(lc3);
+            ligneCommandeService.create(lc4);
+            ligneCommandeService.create(lc5);
+
+
+
+            produitService.afficherProduitsCommande(cmd1.getId());
+
+
+            Date dateDebut = sdf.parse("13/03/2018");
+            Date dateFin = sdf.parse("15/03/2018");
+
+            List<Produit> produitsCommandes = produitService.findProduitsCommandesEntreDates(dateDebut, dateFin);
+            System.out.println("Produits commandés entre le " + sdf.format(dateDebut) + " et le " + sdf.format(dateFin) + " :");
+            System.out.println("Référence       Prix        Quantité" );
+
+            for (Produit p : produitsCommandes) {
+                System.out.println("- " + p.getReference() + " : "  + "       " + p.getPrix() + " DH        "+p.getQuantiteStock());
+            }
+
+
+            List<Produit> produitsChers = produitService.findProduitsPrixSuperieur(100);
+            System.out.println("Produits avec prix > 100 DH :");
+            System.out.println("Référence       Prix        Quantité" );
+
+            for (Produit p : produitsChers) {
+                System.out.println("- " + p.getReference() + " : " +"       " + p.getPrix() + " DH" + "        "+p.getQuantiteStock());
+            }
+
+
+        } catch (Exception e) {
+            System.err.println("Erreur lors des tests : " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            HibernateUtil.shutdown();
+        }
+    }
+}
