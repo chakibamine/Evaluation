0358674de27984bcb006aac47bf247d20f9ea637
chakibamine <aminechakib2003@gmail.com>
2025-10-23 20:31:18 +0100

    exercice 3

--- EvaluationExercice3/src/main/java/ma/projet/App.java
+++ EvaluationExercice3/src/main/java/ma/projet/App.java
@@ -1,4 +1,4 @@
-package com.example;
+package ma.projet;
 
 /**
  * Hello world!
--- EvaluationExercice3/src/test/java/ma/projet/AppTest.java
+++ EvaluationExercice3/src/test/java/ma/projet/AppTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package ma.projet;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
